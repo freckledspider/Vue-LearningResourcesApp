5feae68fc1b9f826d404a5140fc807beda5c0661
bri <bcamesi@gmail.com>
2023-03-13 19:36:18 -0400

    teleport

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,4 +1,5 @@
 <template>
+  <teleport to="">
     <div @click="$emit('close')"></div>
     <dialog open>
         <header>
@@ -15,6 +16,7 @@
             </slot>
         </menu>
     </dialog>
+  </teleport>
 </template>
 
 <script>
