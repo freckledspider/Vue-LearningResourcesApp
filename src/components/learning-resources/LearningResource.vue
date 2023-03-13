65e52d59e4938f74462d9240b16e608e4f98b33b
bri <bcamesi@gmail.com>
2023-03-13 16:05:21 -0400

    dynamic components

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
         <base-card>
             <header>
                 <h3>{{ title }}</h3>
-                <base-button mode="flat">Delete</base-button>
+                <base-button>Delete</base-button>
             </header>
         <p>{{ description }}</p>
         <nav>
