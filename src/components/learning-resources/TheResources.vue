906c5a5f1767b6e637d9e1e6b00d2950330a7452
bri <bcamesi@gmail.com>
2023-03-13 16:43:21 -0400

    fetch user input for new resource

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -3,7 +3,9 @@
         <base-button @click="setSelectedTab('stored-resources')" :mode="storedResButtonMode">Stored Resources</base-button>
         <base-button @click="setSelectedTab('add-resource')" :mode="addResButtonMode">Add Resource</base-button>
     </base-card>
+    <keep-alive>
     <component :is="selectedTab"></component>
+    </keep-alive>
 </template>
 
 <script>
@@ -26,7 +28,8 @@ export default {
     },
     provide() {
         return {
-            resources: this.storedResources
+            resources: this.storedResources,
+            addResource: this.addResource
         }
     },
     computed: {
@@ -40,6 +43,16 @@ export default {
     methods: {
         setSelectedTab(tab) {
             this.selectedTab = tab;
+        },
+        addResource(title, description, url) {
+            const newResource = {
+                id: new Date().toISOString(),
+                title: title,
+                description: description,
+                link: url
+            };
+            this.storedResources.unshift(newResource)
+            this.selectedTab = 'stored-resources'
         }
     }
 }
